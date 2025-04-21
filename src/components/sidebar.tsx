680ddb8660a6043cb5f558b09736fed4eb257567
titanium-dev <akinkunmioye42@gmail.com>
2025-04-21 02:37:56 +0100

    feat: add Sidebar component with styling and layout

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <div className="w-[348px] h-[calc(100vh-55px)] mt-7 bg-white backdrop-blur-lg rounded-3xl p-10">
+      Sidebar
+    </div>
+  );
+};
+
+export default Sidebar;
