46096945ec33cb11768916928ac2df1e8e93552a
titanium-dev <akinkunmioye42@gmail.com>
2025-04-23 01:33:54 +0100

    feat: add AddPassword component for password entry modal

---
+++ src/components/modals/add-password.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddPassword = () => {
+  return (
+    <div className="size-[600px] bg-white rounded-3xl p-4">
+      <h1 className="text-lg font-medium">Add Password</h1>
+    </div>
+  );
+};
+
+export default AddPassword;
